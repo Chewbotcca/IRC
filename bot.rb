97524da994afd7ff9a8bb72868067531717cd9a1
Chewsterchew <chew@chew.pw>
2017-09-03 13:48:05 -0500

    Add new plugins to bot.rb and remove the old cat command.

--- bot.rb
+++ bot.rb
@@ -6,6 +6,8 @@ require 'RestClient'
 require 'json'
 require 'net/http'
 require 'yaml'
+require 'Nokogiri'
+require 'open-uri'
 
 # Load config from file
 CONFIG = YAML.load_file('config.yaml')
@@ -25,18 +27,13 @@ bot = Cinch::Bot.new do
     c.channels = [CONFIG['channels']]
 
     # Plugins to Load, usually you don't need to modify
-    c.plugins.plugins = [UuidLookup, JoinChannel, PartChannel, ServerStatus, Ping, Uptime, Restart]
+    c.plugins.plugins = [UuidLookup, JoinChannel, PartChannel, ServerStatus, Ping, Uptime, Restart, RandomCat, MemeDB]
 
     # Check to see if the user really modified the config.
     if c.nick == '' || c.server == '' || c.channels == ['']
       puts 'You did not configure your bot! Please configure the bot.'
       exit
     end
-  end
-
-  # A command I didn't convert to a plugin yet.
-  on :message, '!cat' do |m|
-    m.reply (JSON.parse(RestClient.get('http://random.cat/meow'))['file']).to_s
   end
 end
 
