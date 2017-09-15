5051548aaaf323a84dc0f1b43687ba527e250cd0
Chewsterchew <chew@chew.pw>
2017-09-14 19:03:09 -0500

    fix typo

--- plugins/nickserv.rb
+++ plugins/nickserv.rb
@@ -17,7 +17,7 @@ class NickServ
       d = YAML.load_file('config.yaml')
       d['nickservpass'] = pass.to_s
       File.open('config.yaml', 'w') { |f| f.write d.to_yaml }
-      m.reply 'I sent verification to nickserv. It might need to verify the email. In that case, type `!nsverify [code]` to verify it. Otherwise, the password has been logged into the config and it will auto-authticate on startup!'
+      m.reply 'I sent verification to nickserv. It might need to verify the email. In that case, type `!nsverify [code]` to verify it. Otherwise, the password has been logged into the config and it will auto-authenticate on startup!'
     else
       m.reply 'You are not permitted to do this action!'
     end
