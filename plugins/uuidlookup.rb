37ae0265174f84a9505ef06375f83e551676f6d5
Chewsterchew <chew@chew.pw>
2017-09-22 15:31:42 -0500

    Improvements to UUID lookup

--- plugins/uuidlookup.rb
+++ plugins/uuidlookup.rb
@@ -3,6 +3,10 @@ class UuidLookup
 
   match /uuid (.+)/, method: :lookforit
   def lookforit(m, name)
-    m.reply "The UUID of #{name} is: #{JSON.parse(RestClient.get("https://api.mojang.com/users/profiles/minecraft/#{name}"))['id']}"
+    if JSON.parse(RestClient.get("https://use.gameapis.net/mc/player/profile/#{name}"))['error'] != nil
+      m.reply "Error: #{JSON.parse(RestClient.get("https://use.gameapis.net/mc/player/profile/#{name}"))['error']} Make sure it is spelled correctly. (It is not case-sensitive)"
+    else
+      m.reply "The UUID of #{JSON.parse(RestClient.get("https://use.gameapis.net/mc/player/profile/#{name}"))['name']} is: #{JSON.parse(RestClient.get("https://use.gameapis.net/mc/player/profile/#{name}"))['uuid_formatted']}"
+    end
   end
 end
