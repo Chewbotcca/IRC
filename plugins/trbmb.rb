9f7612806ae5f7d38cfb729853829eed7d38d4af
Chewsterchew <chew@chew.pw>
2017-09-23 16:36:27 -0500

    fix trbmb plugin for new api

--- plugins/trbmb.rb
+++ plugins/trbmb.rb
@@ -4,6 +4,6 @@ class TRBMB
   match /trbmb/, method: :trbmb
 
   def trbmb(m)
-    m.reply JSON.parse(RestClient.get("https://trbmb.herokuapp.com/"))[0]
+    m.reply JSON.parse(RestClient.get("https://api.chew.pro/trbmb"))[0]
   end
 end
