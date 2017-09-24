4e8862e2a3242b111a312275eb4c5a8955952491
Chewsterchew <chew@chew.pw>
2017-09-23 21:04:54 -0500

    faster cats = happier chew

--- plugins/randomcat.rb
+++ plugins/randomcat.rb
@@ -4,8 +4,12 @@ class RandomCat
   match /cat/, method: :showmethecatto
 
   def showmethecatto(m)
-    jsoncat = JSON.parse(RestClient.get('http://random.cat/meow'))['file']
-    xmlcat = Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
-    m.reply "#{['Aww!', 'Adorable.'].sample} #{[jsoncat, xmlcat].sample}"
+    whichcat = rand(2)
+    if whichcat.zero?
+      showcat = JSON.parse(RestClient.get('http://random.cat/meow'))['file']
+    else
+      showcat = Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
+    end
+    m.reply "#{['Aww!', 'Adorable.'].sample} #{showcat}"
   end
 end
