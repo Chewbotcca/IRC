6b88ed3466226fdb56dbb0be3f1fa9916b288f4b
Chewsterchew <chew@chew.pw>
2017-10-05 16:59:40 -0500

    Fix require gems not working (maybe?)

--- requiregems.rb
+++ requiregems.rb
@@ -15,9 +15,9 @@ begin
   require 'RestClient'
 rescue LoadError
   puts "You're missing the gem `rest-client`. Would you like to install this now? (y/n)"
-  input = gets
+  input = gets.chomp
   if input == 'y'
-    eval `gem install rest-client`
+    `gem install rest-client`
     puts 'Gem installed! Continuing..'
   else
     puts 'To continue, install the rest-client gem'
@@ -31,9 +31,9 @@ begin
   require 'Nokogiri'
 rescue LoadError
   puts "You're missing the gem `nokogiri`. Would you like to install this now? (y/n)"
-  input = gets
+  input = gets.chomp
   if input == 'y'
-    eval `gem install nokogiri`
+    `gem install nokogiri`
     puts 'Gem installed! Continuing..'
   else
     puts 'To continue, install the nokogiri gem'
