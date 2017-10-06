a73ba75309caf4846fab3c0d1ccc1ac9a67f47b3
Chewsterchew <chew@chew.pw>
2017-10-06 16:25:58 -0500

    [Fixes #164] Join serv args, new failover (sorta) for invalid IPs.

--- plugins/serverstatus.rb
+++ plugins/serverstatus.rb
@@ -4,16 +4,21 @@ class ServerStatus
   match /server (.+)/, method: :checkserver
 
   def checkserver(m, serv)
-    m.reply "Server Info for #{serv}:"
-    sleep 0.5
-    if JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['online'] == true
-      m.reply 'Online'
-      sleep 0.5
-      m.reply "Players [Online/Max]: #{JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['players']['online']}/#{JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['players']['max']}"
-      sleep 0.5
-      m.reply "Version: #{JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['version']['name']}"
+    serv = serv.delete(' ')
+    if serv.delete('.') == serv
+      m.reply 'Not a valid address! Server IP SHOULD have dots somewhere.'
     else
-      m.reply 'Offline'
+      m.reply "Server Info for #{serv}:"
+      sleep 0.5
+      if JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['online'] == true
+        m.reply 'Online'
+        sleep 0.5
+        m.reply "Players [Online/Max]: #{JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['players']['online']}/#{JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['players']['max']}"
+        sleep 0.5
+        m.reply "Version: #{JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['version']['name']}"
+      else
+        m.reply 'Offline'
+      end
     end
   end
 end
