794070bb3cf4aea5344d50ce9c06e434abe9cad1
Chewsterchew <chew@chew.pw>
2017-10-12 17:22:59 -0500

    Removed ping plugin and moved it to about.rb

--- plugins/about.rb
+++ plugins/about.rb
@@ -2,8 +2,13 @@ class About
   include Cinch::Plugin
 
   match /about/, method: :about
+  match /ping/, method: :ping
 
   def about(m)
-    m.reply "Chewbotcca is a bot by Chewsterchew. Check it out: https://git.io/ChewbotccaIRC"
+    m.reply 'Chewbotcca is a bot by Chewsterchew. Check it out: https://git.io/ChewbotccaIRC'
+  end
+
+  def ping(m)
+    m.reply 'Hello yes i am alive. ready for action. beep beep boop boop.'
   end
 end
