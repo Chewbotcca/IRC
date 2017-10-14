d2dd185ff0b85e609c415b85aa27c2400f5c10c8
Chewsterchew <chew@chew.pw>
2017-10-14 08:39:14 -0500

    define evalerrors then sample it

--- plugins/eval.rb
+++ plugins/eval.rb
@@ -6,9 +6,10 @@ class Eval
   def execute(m, code)
     if m.user.host == CONFIG['ownerhost']
       begin
-        m.reply (eval code)
+        m.reply eval code
       rescue => e
-        m.reply "#{['Well, excuse me, mr nobrain, cant even eval correctly smh.', "You can't eval? Why are you running a bot!", 'Evaluation error! Please consult your handbook (as in how to properly ruby)', 'The real error here is your lack of ruby knowledge', 'Has halloween come early this year? because your lack of ability to eval is pretty spooky.'].sample} THE ERROR: ```#{e}```"
+        evalerrors = ['Well, excuse me, mr nobrain, cant even eval correctly smh.', "You can't eval? Why are you running a bot!", 'Evaluation error! Please consult your handbook (as in how to properly ruby)', 'The real error here is your lack of ruby knowledge', 'Has halloween come early this year? because your lack of ability to eval is pretty spooky.']
+        m.reply "#{evalerrors.sample} THE ERROR: ```#{e}```"
       end
     else
       m.reply 'An error was thrown (similar to a baseball). It crashed through your house window and had a message written on it. It says: "You do not have permission to evaluate!" Now you have to fix your window.'
