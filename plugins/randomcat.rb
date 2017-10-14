e6356441a2e86acf1ae4681c26346de4a6441dbb
Chewsterchew <chew@chew.pw>
2017-10-14 08:40:07 -0500

    Improved ruby improvements

--- plugins/randomcat.rb
+++ plugins/randomcat.rb
@@ -8,33 +8,29 @@ class RandomCat
 
   def showmethecatto(m)
     whichcat = rand(2)
-    if whichcat.zero?
-      showcat = JSON.parse(RestClient.get('http://random.cat/meow'))['file']
-    else
-      showcat = Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
-    end
+    showcat = if whichcat.zero?
+                JSON.parse(RestClient.get('http://random.cat/meow'))['file']
+              else
+                Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
+              end
     m.reply "#{['Aww!', 'Adorable.'].sample} #{showcat}"
   end
 
   def multikitty(m, amount)
     currentcat = 0
-    caturls = ""
-    if amount.to_i < 1
-      amount = 1
-    end
-    if amount.to_i > 5
-      amount = 5
-    end
+    caturls = ''
+    amount = 1 if amount.to_i < 1
+    amount = 5 if amount.to_i > 5
     while currentcat.to_i < amount.to_i
       whichcat = rand(2)
-      if whichcat.zero?
-        showcat = JSON.parse(RestClient.get('http://random.cat/meow'))['file']
-      else
-        showcat = Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
-      end
+      showcat = if whichcat.zero?
+                  JSON.parse(RestClient.get('http://random.cat/meow'))['file']
+                else
+                  Nokogiri::XML(open('http://thecatapi.com/api/images/get?format=xml&results_per_page=1')).xpath('//url').text
+                end
       caturls = "#{showcat} #{caturls}"
       currentcat += 1
     end
-    m.reply "#{caturls}"
+    m.reply caturls.to_s
   end
 end
