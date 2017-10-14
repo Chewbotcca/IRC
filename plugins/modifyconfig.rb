d30a34245c720358fc9906070b8e535d1763cdc4
Chewsterchew <chew@chew.pw>
2017-10-14 08:39:54 -0500

    Fix nested def definition

--- plugins/modifyconfig.rb
+++ plugins/modifyconfig.rb
@@ -25,15 +25,15 @@ class ModifyConfig
     else
       m.reply 'Incorrect Password!'
     end
+  end
 
-    def addowner(m, pass, owner)
-      if pass == CONFIG['modifypass']
-          CONFIG['ownerhost'] = "#{CONFIG['ownerhost']} || #{m.user.host.to_s}"
-          File.open('config.yaml', 'w') { |f| f.write CONFIG.to_yaml }
-          m.reply "Added `#{owner}` to the list of owners! Run `!restart restartonly` to put changes into action!"
-      else
-        m.reply 'Incorrect Password!'
-      end
+  def addowner(m, pass, owner)
+    if pass == CONFIG['modifypass']
+      CONFIG['ownerhost'] = "#{CONFIG['ownerhost']} || #{m.user.host}"
+      File.open('config.yaml', 'w') { |f| f.write CONFIG.to_yaml }
+      m.reply "Added `#{owner}` to the list of owners! Run `!restart restartonly` to put changes into action!"
+    else
+      m.reply 'Incorrect Password!'
     end
   end
 end
