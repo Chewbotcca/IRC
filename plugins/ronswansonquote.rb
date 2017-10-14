7dec4e3d8a07da5bd9b974296a3138dfe7bd5992
Chewsterchew <chew@chew.pw>
2017-10-14 08:40:21 -0500

    Preferring single quotes over string interpolation

--- plugins/ronswansonquote.rb
+++ plugins/ronswansonquote.rb
@@ -6,6 +6,6 @@ class RonQuote
   match /ron/, method: :getquote
 
   def getquote(m)
-    m.reply "\"#{JSON.parse(RestClient.get("http://ron-swanson-quotes.herokuapp.com/v2/quotes"))[0]}\" -Ron Swanson"
+    m.reply "\"#{JSON.parse(RestClient.get('http://ron-swanson-quotes.herokuapp.com/v2/quotes'))[0]}\" -Ron Swanson"
   end
 end
