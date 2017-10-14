fa76c9786c2cc7dae826f23897128b63e268ca99
Chewsterchew <chew@chew.pw>
2017-10-14 08:40:52 -0500

    using !action.nil? instead of action != nil

--- plugins/uuidlookup.rb
+++ plugins/uuidlookup.rb
@@ -3,7 +3,7 @@ class UuidLookup
 
   match /uuid (.+)/, method: :lookforit
   def lookforit(m, name)
-    if JSON.parse(RestClient.get("https://use.gameapis.net/mc/player/profile/#{name}"))['error'] != nil
+    if !JSON.parse(RestClient.get("https://use.gameapis.net/mc/player/profile/#{name}"))['error'].nil?
       m.reply "Error: #{JSON.parse(RestClient.get("https://use.gameapis.net/mc/player/profile/#{name}"))['error']} Make sure it is spelled correctly. (It is not case-sensitive)"
     else
       m.reply "The UUID of #{JSON.parse(RestClient.get("https://use.gameapis.net/mc/player/profile/#{name}"))['name']} is: #{JSON.parse(RestClient.get("https://use.gameapis.net/mc/player/profile/#{name}"))['uuid_formatted']}"
