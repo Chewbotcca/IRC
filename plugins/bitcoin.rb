94148101b8e86b04c2333f47cf37418bbab1b09c
Chewsterchew <chew@chew.pw>
2017-10-14 08:38:40 -0500

    Remove trailing whitespace

--- plugins/bitcoin.rb
+++ plugins/bitcoin.rb
@@ -7,4 +7,4 @@ class Bitcoin
     grabthatsweetapi = JSON.parse(RestClient.get('https://api.coindesk.com/v1/bpi/currentprice.json'))
     m.reply "Bitcoin Conversions: USD: $#{grabthatsweetapi['bpi']['USD']['rate']}. GBP: £#{grabthatsweetapi['bpi']['GBP']['rate']}. EUR: €#{grabthatsweetapi['bpi']['EUR']['rate']}"
   end
-end 
+end
