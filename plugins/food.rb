32c44258c51d7da247fa4ecb9bbaeeee813b5aed
Chewsterchew <chew@chew.pw>
2017-10-14 08:39:23 -0500

    define taco types and sample them

--- plugins/food.rb
+++ plugins/food.rb
@@ -1,10 +1,12 @@
 class Food
   include Cinch::Plugin
 
-  match /taco (.+)/, method: :destination
+  match /taco (.+)/, method: :taco
 
-  def destination(m, user)
-    toppings = ['guacamole','salsa','sour cream','cheese','lettuce','tomatoes','avocado','onion','scallions','jalepeños','capsicum','ghost chili','olives','pineapple','raspberries']
-    m.reply "#{m.user.nick} gives #{user} a #{['spicy','mild','tasty','delicious','boring','disgusting','perfect'].sample} taco filled with #{['minced beef','shredded beef','steak','pork','various meats','chicken','refried beans','tofu'].sample} with #{toppings.sample}, #{toppings.sample}, and #{toppings.sample}!"
+  def taco(m, user)
+    toppings = ['guacamole', 'salsa', 'sour cream', 'cheese', 'lettuce', 'tomatoes', 'avocado', 'onion', 'scallions', 'jalepeños', 'capsicum', 'ghost chili', 'olives', 'pineapple', 'raspberries']
+    tacotype = %w(spicy mild tasty delicious boring disgusting perfect)
+    meat = ['minced beef', 'shredded beef', 'steak', 'pork', 'various meats', 'chicken', 'refried beans', 'tofu']
+    m.reply "#{m.user.nick} gives #{user} a #{tacotype.sample} taco filled with #{meat.sample} with #{toppings.sample}, #{toppings.sample}, and #{toppings.sample}!"
   end
 end
