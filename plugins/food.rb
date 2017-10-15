f0c3fed7e7a478bad3744662811b63f7c603f4ba
Chewsterchew <chew@chew.pw>
2017-10-15 16:05:52 -0500

    [Fixes #87] add a weird potato command.

--- plugins/food.rb
+++ plugins/food.rb
@@ -3,6 +3,7 @@ class Food
 
   match /taco (.+)/, method: :taco
   match /cookie (.+)/, method: :cookie
+  match /potato (.+)/, method: :potato
 
   def taco(m, user)
     toppings = ['guacamole', 'salsa', 'sour cream', 'cheese', 'lettuce', 'tomatoes', 'avocado', 'onion', 'scallions', 'jalepeños', 'capsicum', 'ghost chili', 'olives', 'pineapple', 'raspberries']
@@ -18,5 +19,14 @@ class Food
     methods = %w(makes gives gets buys)
     side_dish = ['glass of milk', 'bowl of ice cream', 'bowl of chocolate sauce']
     m.reply "#{m.user.nick} #{methods.sample} #{user} a #{flavour.sample} #{size.sample} #{cookies.sample} cookie and serves it with a #{side_dish.sample}"
+  end
+
+  def potato(m, user)
+    potatoes = ['Laura', 'Kind Edward', 'Kerr\'s Pink', 'Sweet', 'Normal']
+    size = ['small', 'little', 'mid-sized', 'medium-sized', 'large', 'gigantic']
+    flavour = %w(tasty delectable delicious yummy toothsome scrumptious luscious)
+    method = %w(bakes fries boils roasts)
+    side_dish = ['side salad', 'dollop of sour cream', 'piece of chicken', 'bowl of shredded bacon']
+    m.reply "#{m.user.nick} #{method.sample} a #{flavour.sample} #{size.sample} #{potatoes.sample} potato for #{user} and serves it with a small #{side_dish.sample}"
   end
 end
