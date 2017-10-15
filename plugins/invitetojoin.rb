7ec8c6cd0284f44080838c4fc25a489845b4dd51
Chewsterchew <chew@chew.pw>
2017-10-15 15:33:44 -0500

    Add !leave command, allows staff to force the bot to leave until invited

--- plugins/invitetojoin.rb
+++ plugins/invitetojoin.rb
@@ -2,6 +2,7 @@ class InviteToJoin
   include Cinch::Plugin
 
   listen_to :invite, method: :invite
+  match /leave/, method: :leave
 
   def invite(m)
     if CONFIG['channels'] != ''
@@ -17,5 +18,20 @@ class InviteToJoin
     end
     File.open('config.yaml', 'w') { |f| f.write CONFIG.to_yaml }
     Channel(m.channel.to_s).join
+  end
+
+  def leave(m)
+    if m.channel.half_ops.join(' ').split(' ').include?(m.user.nick) || m.channel.ops.join(' ').split(' ').include?(m.user.nick) || m.channel.admins.join(' ').split(' ').include?(m.user.nick) || m.channel.owners.join(' ').split(' ').include?(m.user.nick)
+      CONFIG['channels'] = "#{CONFIG['channels']},#r28trgh9247tg24h9"
+      CONFIG['channels'] = (CONFIG['channels']).to_s.sub("#{m.channel},", '')
+      CONFIG['channels'] = (CONFIG['channels']).to_s.sub(',,', ',')
+      CONFIG['channels'] = (CONFIG['channels']).to_s.sub('#r28trgh9247tg24h9', '')
+      CONFIG['channels'] = (CONFIG['channels']).to_s.chomp(',')
+      File.open('config.yaml', 'w') { |f| f.write CONFIG.to_yaml }
+      m.reply 'Bye!'
+      Channel(m.channel, 'Quit: Requested to leave.').part
+    else
+      m.reply 'Only half-ops and above may force me to leave!'
+    end
   end
 end
