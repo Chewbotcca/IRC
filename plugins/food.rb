f29197ceabfb00e382454f00553943332bc8de3c
Chewsterchew <chew@chew.pw>
2017-10-16 14:40:24 -0500

    Revert those ()s, I played myself

--- plugins/food.rb
+++ plugins/food.rb
@@ -10,7 +10,7 @@ class Food
 
   def taco(m, user)
     toppings = ['guacamole', 'salsa', 'sour cream', 'cheese', 'lettuce', 'tomatoes', 'avocado', 'onion', 'scallions', 'jalepeños', 'capsicum', 'ghost chili', 'olives', 'pineapple', 'raspberries']
-    tacotype = %w[spicy mild tasty delicious boring disgusting perfect]
+    tacotype = %w(spicy mild tasty delicious boring disgusting perfect)
     meat = ['minced beef', 'shredded beef', 'steak', 'pork', 'various meats', 'chicken', 'refried beans', 'tofu']
     m.reply "#{m.user.nick} gives #{user} a #{tacotype.sample} taco filled with #{meat.sample} with #{toppings.sample}, #{toppings.sample}, and #{toppings.sample}!"
   end
@@ -18,8 +18,8 @@ class Food
   def cookie(m, user)
     cookies = ['Chocolate Chip', 'Oatmeal', 'Sugar', 'Oatmeal Raisin', 'Macadamia Nut', 'Jam Thumbprint', 'Medican Wedding', 'Biscotti', 'Oatmeal Cranberry', 'Chocolate Fudge', 'Peanut Butter', 'Pumpkin', 'Lemon Bar', 'Chocolate Oatmeal Fudge', 'Toffee Peanut', 'Danish Sugar', 'Triple Chocolate', 'Oreo']
     size = ['small', 'little', 'medium-sized', 'large', 'gigantic']
-    flavour = %w[tasty delectable delicious yummy toothsome scrumptious luscious]
-    methods = %w[makes gives gets buys]
+    flavour = %w(tasty delectable delicious yummy toothsome scrumptious luscious)
+    methods = %w(makes gives gets buys)
     side_dish = ['glass of milk', 'bowl of ice cream', 'bowl of chocolate sauce']
     m.reply "#{m.user.nick} #{methods.sample} #{user} a #{flavour.sample} #{size.sample} #{cookies.sample} cookie and serves it with a #{side_dish.sample}"
   end
@@ -27,8 +27,8 @@ class Food
   def potato(m, user)
     potatoes = ['Laura', 'Kind Edward', 'Kerr\'s Pink', 'Sweet', 'Normal']
     size = ['small', 'little', 'mid-sized', 'medium-sized', 'large', 'gigantic']
-    flavour = %w[tasty delectable delicious yummy toothsome scrumptious luscious]
-    method = %w[bakes fries boils roasts]
+    flavour = %w(tasty delectable delicious yummy toothsome scrumptious luscious)
+    method = %w(bakes fries boils roasts)
     side_dish = ['side joey', 'dollop of sour cream', 'piece of chicken', 'bowl of shredded bacon']
     m.reply "#{m.user.nick} #{method.sample} a #{flavour.sample} #{size.sample} #{potatoes.sample} potato for #{user} and serves it with a small #{side_dish.sample}"
   end
@@ -36,8 +36,8 @@ class Food
   def cake(m, user)
     cakes = ['Chocolate', 'Ice Cream', 'Angel', 'Boston Cream', 'Birthday', 'Bundt', 'Carrot', 'Coffee', 'Devils', 'Fruit', 'Gingerbread', 'Pound', 'Red Velvet', 'Stack', 'Welsh', 'Yokan']
     size = ['small', 'little', 'mid-sized', 'medium-sized', 'large', 'gigantic']
-    flavour = %w[tasty delectable delicious yummy toothsome scrumptious luscious]
-    method = %w[makes gives gets buys]
+    flavour = %w(tasty delectable delicious yummy toothsome scrumptious luscious)
+    method = %w(makes gives gets buys)
     side_dish = ['glass of chocolate milk', 'bowl of ice cream', 'jar of cookies', 'some chocolate sauce']
     m.reply "#{m.user.nick} #{method.sample} #{user} a #{flavour.sample} #{size.sample} #{cakes.sample} cake and serves it with a small #{side_dish.sample}"
   end
@@ -52,8 +52,8 @@ class Food
   def sandwich(m, user)
     meat = ['ham', 'salami', 'steak', 'meatball', 'tuna', 'pork', 'chicken', 'roast beef', 'corned beef', 'pastrami', 'turkey']
     flavour = ['a tasty', 'a delicious', 'an awesome', 'an excellent', 'a beautifully-made']
-    type = %w[roll wrap pita sandwich bun]
-    method = %w[hands gives makes passes]
+    type = %w(roll wrap pita sandwich bun)
+    method = %w(hands gives makes passes)
     joey = ['tomatoes', 'lettuce', 'pickles', 'cucumbers', 'red onions', 'jalapenos']
     sauce = ['sweet onion sauce', 'honey mustard sauce', 'mayo', 'sweet chili sauce', 'italian sauce', 'ranch dressing', 'barbecue sauce']
     andmeat = ["and #{meat.sample}", '']
