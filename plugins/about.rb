15a3006b35d4251a30db88483d27b650e755c36f
Chewsterchew <chew@chew.pw>
2017-10-22 08:36:51 -0500

    add bug reporting command (for bot owners)

--- plugins/about.rb
+++ plugins/about.rb
@@ -4,6 +4,9 @@ class About
   match /about/, method: :about
   match /ping/, method: :ping
   match /uptime/, method: :uptime
+  match /bug (.+)/, method: :bug
+  match /suggestion (.+)/, method: :bug
+  match /issue (.+)/, method: :bug
 
   def about(m)
     m.reply 'Chewbotcca is a bot by Chewsterchew. Check it out: https://git.io/ChewbotccaIRC'
@@ -19,5 +22,17 @@ class About
     hh, mm = mm.divmod(60)
     dd, hh = hh.divmod(24)
     m.reply format('Uptime: %d days, %d hours, %d minutes and %d seconds', dd, hh, mm, ss)
+  end
+
+  def bug(m, args)
+    if CONFIG['ownerhost'] == m.user.host
+      begin
+        m.reply "Issue created! #{`hub issue create -m "#{args}"`}"
+      rescue
+        m.reply 'There was an error making the issue. Do you have hub installed? Did you `git clone`? Does git know who you are?'
+      end
+    else
+      m.reply 'You must be the bot owner to create an issue! Try making one manually: http://github.com/Chewbotcca/IRC/issues/new'
+    end
   end
 end
