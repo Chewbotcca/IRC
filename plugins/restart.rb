3973ae993e4b81b64a5b3109d5bdce8b841a3823
Chewsterchew <chew@chew.pw>
2017-10-22 18:13:52 -0500

    add update.rb and use exec ruby instead of using a bash script

--- plugins/restart.rb
+++ plugins/restart.rb
@@ -2,21 +2,26 @@ class Restart
   include Cinch::Plugin
 
   match /restart/, method: :restart
+  match /update/, method: :update
 
   def restart(m)
     if m.user.host == CONFIG['ownerhost']
-      if CONFIG['os'] == 'Windows'
-        m.reply 'Restarting not supported on Windows!'
-      end
-      if CONFIG['os'] == 'Mac' || CONFIG['os'] == 'Linux'
-        begin
-          m.reply 'Restarting the bot without updating...'
-          sleep 1
-          exec('bash scripts/restartonly.sh')
-        end
-      end
+      m.reply 'Restarting the bot without updating...'
+      sleep 1
+      exec('ruby bot.rb')
     else
       m.reply "You can't restart! (If you are the owner of the bot, you did not configure properly! Otherwise, stop trying to restart the bot!)"
+    end
+  end
+
+  def update(m)
+    if m.user.host == CONFIG['ownerhost']
+      m.reply 'Restarting and Updating!'
+      sleep 1
+      `git pull`
+      exec('ruby bot.rb')
+    else
+      m.reply "You can't update! (If you are the owner of the bot, you did not configure properly! Otherwise, stop trying to update the bot!)"
     end
   end
 end
