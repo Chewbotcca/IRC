bea55d867f56e3a9018d551d86b5aadc62e212a4
Chewsterchew <chew@chew.pw>
2017-10-22 17:26:14 -0500

    use previous commit code for inviting the bot to join

--- plugins/invitetojoin.rb
+++ plugins/invitetojoin.rb
@@ -5,17 +5,9 @@ class InviteToJoin
   match /leave/, method: :leave
 
   def invite(m)
-    if CONFIG['channels'] != ''
-      CONFIG['channels'] = "#{CONFIG['channels']},#r28trgh9247tg24h9"
-      CONFIG['channels'] = (CONFIG['channels']).to_s.sub("#{m.channel},", '')
-      CONFIG['channels'] = (CONFIG['channels']).to_s.sub(',,', ',')
-      CONFIG['channels'] = (CONFIG['channels']).to_s.sub('#r28trgh9247tg24h9', '')
-      CONFIG['channels'] = (CONFIG['channels']).to_s.chomp(',')
-      CONFIG['channels'] = "#{CONFIG['channels']},#{m.channel}"
-      CONFIG['channels'] = (CONFIG['channels']).to_s.sub(',,', ',')
-    else
-      CONFIG['channels'] = m.channel.to_s
-    end
+    channels = CONFIG['channels'].split(',')
+    channels += [m.channel]
+    CONFIG['channels'] = channels.join(',')
     File.open('config.yaml', 'w') { |f| f.write CONFIG.to_yaml }
     Channel(m.channel.to_s).join
   end
