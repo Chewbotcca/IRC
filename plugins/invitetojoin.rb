7ec3e010d4edd8644c15b8561b6d65de1ced05ef
Chewsterchew <chew@chew.pw>
2017-10-22 17:23:35 -0500

    use code from previous commit for !leave

--- plugins/invitetojoin.rb
+++ plugins/invitetojoin.rb
@@ -22,11 +22,9 @@ class InviteToJoin
 
   def leave(m)
     if m.channel.half_ops.join(' ').split(' ').include?(m.user.nick) || m.channel.ops.join(' ').split(' ').include?(m.user.nick) || m.channel.admins.join(' ').split(' ').include?(m.user.nick) || m.channel.owners.join(' ').split(' ').include?(m.user.nick)
-      CONFIG['channels'] = "#{CONFIG['channels']},#r28trgh9247tg24h9"
-      CONFIG['channels'] = (CONFIG['channels']).to_s.sub("#{m.channel},", '')
-      CONFIG['channels'] = (CONFIG['channels']).to_s.sub(',,', ',')
-      CONFIG['channels'] = (CONFIG['channels']).to_s.sub('#r28trgh9247tg24h9', '')
-      CONFIG['channels'] = (CONFIG['channels']).to_s.chomp(',')
+      channels = CONFIG['channels'].split(',')
+      channels -= [part]
+      CONFIG['channels'] = channels.join(',')
       File.open('config.yaml', 'w') { |f| f.write CONFIG.to_yaml }
       m.reply 'Bye!'
       Channel(m.channel).part
