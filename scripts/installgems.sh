570330f0eb32ffab14a6c19c397d5a2b15401881
Chew <Chewsterchew@users.noreply.github.com>
2017-10-23 09:17:35 -0500

    remove while loop, why it exists I don't know
    sorry while loop. rip

--- scripts/installgems.sh
+++ scripts/installgems.sh
@@ -1,6 +1,3 @@
-while true
-do
-  echo "Installing those gems"
-  gem install cinch rest-client nokogiri
-  echo "All good! Run ruby bot.rb"
-done
+echo "Installing those gems"
+gem install cinch rest-client nokogiri
+echo "All good! Run ruby bot.rb"
