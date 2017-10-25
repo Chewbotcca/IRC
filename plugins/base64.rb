78f83d64b3dc6cc2f27a2d9a45c42826ac0c8f0d
Chewsterchew <chew@chew.pw>
2017-10-24 21:09:18 -0500

    [Fixes #25] Add base64 decoder command.

--- plugins/base64.rb
+++ plugins/base64.rb
@@ -2,8 +2,18 @@ class BaseS4
   include Cinch::Plugin
 
   match /base64 (.+)/, method: :base64
+  match /debase64 (.+)/, method: :debase
 
   def base64(m, args)
     m.reply Base64.encode64(args)
+  end
+
+  def debase(m, args)
+    decoded = Base64.decode64(args)
+    if decoded == ""
+      m.reply 'Boi whatchu doin? that aint base64! get outta here wit yo basealphabet nonsense.'
+    else
+      m.reply decoded
+    end
   end
 end
