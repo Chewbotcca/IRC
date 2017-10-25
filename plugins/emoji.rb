ebba109a83d7b6f2fe0a2612eef2687879812a53
Chewsterchew <chew@chew.pw>
2017-10-25 16:51:58 -0500

    add 👏 clap 👏 command

--- plugins/emoji.rb
+++ plugins/emoji.rb
@@ -4,8 +4,15 @@ class Emoji
   match /b (.+)/, method: :b
   match /:b: (.+)/, method: :b
   match /🅱️ (.+)/, method: :b
+  match /clap (.+)/, method: :clap
+  match /:clap: (.+)/, method: :clap
+  match /👏 (.+)/, method: :clap
 
   def b(m, args)
     m.reply args.gsub('b', '🅱️').gsub('B', '🅱️')
+  end
+
+  def clap(m, args)
+    m.reply args.gsub(' ','👏')
   end
 end
