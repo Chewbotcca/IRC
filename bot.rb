9a7c71f1b4e2f0ec16f298d18656a1d5c018db10
Chewsterchew <chew@chew.pw>
2017-11-01 13:41:12 -0500

    fix SOME rubocop errors

--- bot.rb
+++ bot.rb
@@ -18,44 +18,49 @@ Dir["#{File.dirname(__FILE__)}/plugins/*.rb"].each { |file| require file }
 # Set up uptime.
 STARTTIME = Time.now
 
+# Pre-Config
+botnick = if CONFIG['nickname'] == '' || CONFIG['nickname'].nil?
+            puts 'The bot doesn\'t have a nickname! Please set one'
+            exit
+          else
+            CONFIG['nickname'].to_s
+          end
+
+botserver = if CONFIG['server'] == '' || CONFIG['server'].nil?
+              puts 'You did not configure a server for the bot to connect to. Please set one!'
+              exit
+            else
+              CONFIG['server'].to_s
+            end
+
+botport = if CONFIG['port'].nil?
+            '6667'
+          else
+            CONFIG['port']
+          end
+
+botuser = if CONFIG['username'].nil? || CONFIG['username'] == ''
+            CONFIG['nickname']
+          else
+            CONFIG['username']
+          end
+
+botrealname = if CONFIG['realname'].nil? || CONFIG['realname'] == ''
+                'Chewbotcca IRC Bot - https://git.io/ChewbotccaIRC'
+              else
+                "#{CONFIG['realname']} - https://git.io/ChewbotccaIRC"
+              end
+
 # Configure the Bot
 bot = Cinch::Bot.new do
   configure do |c|
-
-    # Bot Settings, Taken from Config.yaml
-    c.nick = if CONFIG['nickname'] == '' || CONFIG['nickname'].nil?
-               puts 'The bot doesn\'t have a nickname! Please set one'
-               exit
-             else
-               CONFIG['nickname'].to_s
-             end
-
-    c.server = if CONFIG['server'] == '' || CONFIG['server'].nil?
-                 puts 'You did not configure a server for the bot to connect to. Please set one!'
-                 exit
-               else
-                 CONFIG['server'].to_s
-               end
-
+    # Bot Settings, Taken from pre-config
+    c.nick = botnick
+    c.server = botserver
     c.channels = [CONFIG['channels']]
-
-    c.port = if CONFIG['port'].nil?
-               '6667'
-             else
-               CONFIG['port']
-             end
-
-    c.user = if CONFIG['username'].nil? || CONFIG['username'] == ''
-               CONFIG['nickname']
-             else
-               CONFIG['username']
-             end
-             
-    c.realname = if CONFIG['realname'].nil? || CONFIG['realname'] == ''
-                   'Chewbotcca IRC Bot - https://git.io/ChewbotccaIRC'
-                 else
-                   "#{CONFIG['realname']} - https://git.io/ChewbotccaIRC"
-                 end
+    c.port = botport
+    c.user = botuser
+    c.realname = botrealname
 
     # Load modules.
     c.plugins.plugins = [Minecraft, Owner, Restart, RandomCat, MemeDB, TRBMB, ModifyConfig, NickServ, InviteToJoin, RonQuote, EBall, Bitcoin, QRCode, About, Define, Synonym, Antonym, WordRandom, Emoji, Food, TableFlip, Grammar, Formatting, BaseS4, Choose]
