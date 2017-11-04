d22a0c04c61fd680d6a8462d6b01a105aa94243e
Chewsterchew <chew@chew.pw>
2017-11-04 12:12:08 -0500

    reduce 3 api calls to 1 for !server

--- plugins/minecraft.rb
+++ plugins/minecraft.rb
@@ -65,13 +65,14 @@ class Minecraft
       m.reply 'Not a valid address! Server IP SHOULD have dots somewhere.'
     else
       m.reply "Server Info for #{serv}:"
+      info = JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))
       sleep 0.5
-      if JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['online'] == true
+      if info['online'] == true
         m.reply 'Online'
         sleep 0.5
-        m.reply "Players [Online/Max]: #{JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['players']['online']}/#{JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['players']['max']}"
+        m.reply "Players [Online/Max]: #{info['players']['online']}/#{info['players']['max']}"
         sleep 0.5
-        m.reply "Version: #{JSON.parse(RestClient.get("https://eu.mc-api.net/v3/server/ping/#{serv}"))['version']['name']}"
+        m.reply "Version: #{info['version']['name']}"
       else
         m.reply 'Offline'
       end
