6050c142c7116eb1de7d0ddbca7ba4da031d4d11
Chewsterchew <chew@chew.pw>
2017-11-05 20:54:48 -0600

    we must d e s t r o y advaith specifically

--- plugins/flip.rb
+++ plugins/flip.rb
@@ -6,7 +6,7 @@ class Flip
 
   def flip(m)
     first = ['I flipped a coin, and it landed on', 'I threw the coin into the air and it finally landed on', 'I dropped the coin, it landed on'].sample
-    headtails = %w[Heads Tails].sample
+    headtails = Format(:bold, Format(:italic %w[heads tails].sample))
     m.reply "#{first} #{headtails}"
   end
 end
