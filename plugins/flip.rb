877b3dd553c86d20688e90ae053bbdafbb175eef
Chewsterchew <chew@chew.pw>
2017-11-05 20:37:50 -0600

    remove excess spaces.

--- plugins/flip.rb
+++ plugins/flip.rb
@@ -5,7 +5,7 @@ class Flip
   match /coin/, method: :flip
 
   def flip(m)
-    first = ['I flipped a coin, and it landed on ', 'I threw the coin into the air and it finally landed on ', 'I dropped the coin, it landed on '].sample
+    first = ['I flipped a coin, and it landed on', 'I threw the coin into the air and it finally landed on', 'I dropped the coin, it landed on'].sample
     headtails = %w[Heads Tails].sample
     m.reply "#{first} #{headtails}"
   end
