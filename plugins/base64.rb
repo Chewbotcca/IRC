41d9e1a0e3049b7cfac5957eb83bdcf8355324e5
Chewsterchew <chew@chew.pw>
2017-11-17 19:58:55 -0600

    [Fixes #51] Added !isbase64 command? Not even sure why

--- plugins/base64.rb
+++ plugins/base64.rb
@@ -3,6 +3,16 @@ class BaseS4
 
   match /base64 (.+)/, method: :base64
   match /debase64 (.+)/, method: :debase
+  match /isbase64 (.+)/, method: :isbase
+
+  def isbase(m, args)
+    decoded = Base64.decode64(args)
+    if decoded == ''
+      m.reply "String \"#{args}\" is not base64!"
+    else
+      m.reply "String \"#{args}\" is base64!"
+    end
+  end
 
   def base64(m, args)
     m.reply Base64.encode64(args)
