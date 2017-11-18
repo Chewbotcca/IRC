ad7400835dfc9969e8fcd203781f9cdf3b6dca53
Chewsterchew <chew@chew.pw>
2017-11-17 22:21:59 -0600

    [Fixes #84] Add piglatin command

---
+++ plugins/language.rb
@@ -0,0 +1,9 @@
+class Language
+  include Cinch::Plugin
+
+  match /piglatin (.+)/, method: :piglatin
+
+  def piglatin(m, args)
+    m.reply JSON.parse(RestClient.post("http://api.funtranslations.com/translate/piglatin.json", { 'text' =>'mgo' }.to_json, content_type: :json))['contents']['translated']
+  end
+end
