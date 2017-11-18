fb960b49cc055eb0686911e9c880c71432666a69
Chewsterchew <chew@chew.pw>
2017-11-18 09:53:45 -0600

    Fix rubocop errors due to piglatin being weird

--- plugins/language.rb
+++ plugins/language.rb
@@ -4,6 +4,6 @@ class Language
   match /piglatin (.+)/, method: :piglatin
 
   def piglatin(m, args)
-    m.reply JSON.parse(RestClient.post("http://api.funtranslations.com/translate/piglatin.json", { 'text' =>'mgo' }.to_json, content_type: :json))['contents']['translated']
+    m.reply JSON.parse(RestClient.post('http://api.funtranslations.com/translate/piglatin.json', { 'text' => args }.to_json, content_type: :json))['contents']['translated']
   end
 end
