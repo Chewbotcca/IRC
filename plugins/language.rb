86d8f53ccde53c2c45057e9c82446cfe79c49b1e
Chewsterchew <chew@chew.pw>
2017-11-18 13:42:31 -0600

    [Fixes #61] Add leet command.

--- plugins/language.rb
+++ plugins/language.rb
@@ -2,6 +2,11 @@ class Language
   include Cinch::Plugin
 
   match /piglatin (.+)/, method: :piglatin
+  match /leet (.+)/, method: :leet
+
+  def leet(m, args)
+    m.reply JSON.parse(RestClient.post('http://api.funtranslations.com/translate/leetspeak.json', { 'text' => args }.to_json, content_type: :json))['contents']['translated']
+  end
 
   def piglatin(m, args)
     m.reply JSON.parse(RestClient.post('http://api.funtranslations.com/translate/piglatin.json', { 'text' => args }.to_json, content_type: :json))['contents']['translated']
