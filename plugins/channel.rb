16da000acd44fecd72f5b36c517d50093644c8f7
Chewsterchew <chew@chew.pw>
2017-11-19 22:23:42 -0600

    [Fixes #91] It works! Add a quote command. Recall the past. yikes?

--- plugins/channel.rb
+++ plugins/channel.rb
@@ -3,6 +3,29 @@ class Channel
 
   listen_to :channel, method: :log_public_message, strip_colors: true
   match /resethistory/, method: :clearlog
+  match /quote/, method: :quote
+
+  def quote(m)
+    channel = m.channel.to_s[1..m.channel.to_s.length]
+    filename = "data/logs/#{channel}.yaml"
+    unless File.exist?(filename)
+      File.new(filename, 'w+')
+      exconfig = YAML.load_file('data/logs/logs.example.yaml')
+      File.open(filename, 'w') { |f| f.write exconfig.to_yaml }
+      m.reply 'There is no log! No log = No quotes! Try again, bucko'
+      return
+    end
+    data = false
+    while data == false
+      data = YAML.load_file(filename)
+    end
+    count = data['count']
+    ichoose = rand(1..count)
+    colon = data[ichoose].index(':')
+    removed = data[ichoose][colon + 2..data[ichoose].length]
+    user = data[ichoose][0..colon - 1]
+    m.reply "Long ago, #{user} said: \"#{removed}\""
+  end
 
   def getrank(m, user)
     return 'owner' if m.channel.owners.join(' ').split(' ').include?(user)
