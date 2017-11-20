c014bddc3a42cffd8189b1fcd298af5f74a6da13
Chewsterchew <chew@chew.pw>
2017-11-19 22:03:01 -0600

    Rememeber to actually git push the plugin .-.

---
+++ plugins/streams.rb
@@ -0,0 +1,22 @@
+class Streams
+  include Cinch::Plugin
+
+  match /mixer (.+)/, method: :mixer
+
+  def mixer(m, user)
+    user.delete!(' ')
+    begin
+      parse = JSON.parse(RestClient.get("https://mixer.com/api/v1/channels/#{user}"))
+    rescue
+      m.reply 'User not found!'
+      return
+    end
+    if parse['online']
+      online = Format(:green, 'Currently Streaming!')
+    else
+      online = Format(:red, 'Currently Offline.')
+    end
+    followers = parse['numFollowers']
+    m.reply "Info for Mixer user #{Format(:bold, user)}. #{online} Followers: #{Format(:bold, followers}! Stream Title: #{Format(:bold, parsed['name'])}. Total Views: #{Format(:bold, parsed['viewersTotal'])}. URL: http://mixer.com/#{user}"
+  end
+end
