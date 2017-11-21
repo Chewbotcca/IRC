6f33e9a0712db876dbae4c78e7b4ea46d7255aae
Chewsterchew <chew@chew.pw>
2017-11-21 16:25:10 -0600

    [Fixes #172] Add a check to see if there is a wordnik api key

--- plugins/english.rb
+++ plugins/english.rb
@@ -7,56 +7,58 @@ class English
   match /synonym (.+)/, method: :synonym
   match /antonym (.+)/, method: :antonym
 
+  def checkapi
+    if CONFIG['wordnik'] == '' || CONFIG['wordnik'].nil?
+      'false'
+    else
+      'true'
+    end
+  end
+
   def antonym(m, word)
+    unless checkapi == 'true'
+      m.reply 'This command requires an API key from wordnik'
+      return
+    end
     grabbedword = JSON.parse(RestClient.get("http://api.wordnik.com/v4/word.json/#{word}/relatedWords?useCanonical=false&relationshipTypes=antonym&limitPerRelationshipType=10&api_key=#{CONFIG['wordnik']}"))
     begin
-      if grabbedword['message'] == 'unauthorized'
-        m.reply 'This requires an API key from wordnik.com'
-      end
+      m.reply "Antonym(s) for #{word}: #{grabbedword[0]['words'].join(', ')}"
     rescue
-      begin
-        m.reply "Antonym(s) for #{word}: #{grabbedword[0]['words'].join(', ')}"
-      rescue
-        m.reply 'Word not found (or there were no Antonyms) Check your local spell-checker!'
-      end
+      m.reply 'Word not found (or there were no Antonyms) Check your local spell-checker!'
     end
   end
 
   def synonym(m, word)
+    unless checkapi == 'true'
+      m.reply 'This command requires an API key from wordnik'
+      return
+    end
     grabbedword = JSON.parse(RestClient.get("http://api.wordnik.com/v4/word.json/#{word}/relatedWords?useCanonical=false&relationshipTypes=synonym&limitPerRelationshipType=10&api_key=#{CONFIG['wordnik']}"))
     begin
-      if grabbedword['message'] == 'unauthorized'
-        m.reply 'This requires an API key from wordnik.com'
-      end
+      m.reply "Synonym(s) for #{word}: #{grabbedword[0]['words'].join(', ')}"
     rescue
-      begin
-        m.reply "Synonym(s) for #{word}: #{grabbedword[0]['words'].join(', ')}"
-      rescue
-        m.reply 'Word not found (or there were no Synonyms) Check your local spell-checker!'
-      end
+      m.reply 'Word not found (or there were no Synonyms) Check your local spell-checker!'
     end
   end
 
   def randomword(m)
-    if grabbedword['message'] == 'unauthorized'
-      m.reply 'This requires an API key from wordnik.com'
+    unless checkapi == 'true'
+      m.reply 'This command requires an API key from wordnik'
+      return
     end
-  rescue
     m.reply "Random Word: #{JSON.parse(RestClient.get("http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=true&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=-1&api_key=#{CONFIG['wordnik']}"))['word']}"
   end
 
   def define(m, word)
+    unless checkapi == 'true'
+      m.reply 'This command requires an API key from wordnik'
+      return
+    end
     grabbedword = JSON.parse(RestClient.get("http://api.wordnik.com/v4/word.json/#{word}/definitions?limit=1&includeRelated=true&useCanonical=false&includeTags=false&api_key=#{CONFIG['wordnik']}"))
     begin
-      if grabbedword['message'] == 'unauthorized'
-        m.reply 'This requires an API key from wordnik.com'
-      end
+      m.reply "Defintion for #{word}: #{grabbedword[0]['partOfSpeech']}. #{grabbedword[0]['text']}"
     rescue
-      begin
-        m.reply "Defintion for #{word}: #{grabbedword[0]['partOfSpeech']}. #{grabbedword[0]['text']}"
-      rescue
-        m.reply 'Word not found! Check your local spell-checker!'
-      end
+      m.reply 'Word not found! Check your local spell-checker!'
     end
   end
 end
