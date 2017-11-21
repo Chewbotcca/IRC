2747cbb6f985273d468606832e8d20a7febab916
Chewsterchew <chew@chew.pw>
2017-11-20 22:15:06 -0600

    Round the ping ms

--- plugins/about.rb
+++ plugins/about.rb
@@ -13,7 +13,7 @@ class About
   end
 
   def ping(m)
-    m.reply "Hello yes i am alive. ready for action. beep beep boop boop. Oh by the way that ping took #{(Time.now - m.time) * 1000.to_i} ms."
+    m.reply "Hello yes i am alive. ready for action. beep beep boop boop. Oh by the way that ping took #{((Time.now - m.time) * 1000.to_i).round(2)} ms."
   end
 
   def uptime(m)
