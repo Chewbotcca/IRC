406a683151664b26e8a68ec00f43709459ff51f3
Chewsterchew <chew@chew.pw>
2017-11-21 15:19:01 -0600

    [Fixes #187] Mixer API now fixes the capitalization of the user

--- plugins/streams.rb
+++ plugins/streams.rb
@@ -11,12 +11,13 @@ class Streams
       m.reply 'User not found!'
       return
     end
+    name = parse['token']
     online = if parse['online']
                Format(:green, 'Currently Streaming!')
              else
                Format(:red, 'Currently Offline')
              end
     followers = parse['numFollowers'].to_s
-    m.reply "Info for Mixer user #{Format(:bold, user)} | #{Format(:bold, online)} | Followers: #{Format(:bold, followers)} | Stream Title: #{Format(:bold, parse['name'])} | Total Views: #{Format(:bold, parse['viewersTotal'].to_s)} | URL: http://mixer.com/#{user}"
+    m.reply "Info for Mixer user #{Format(:bold, name)} | #{Format(:bold, online)} | Followers: #{Format(:bold, followers)} | Stream Title: #{Format(:bold, parse['name'])} | Total Views: #{Format(:bold, parse['viewersTotal'].to_s)} | URL: http://mixer.com/#{name}"
   end
 end
