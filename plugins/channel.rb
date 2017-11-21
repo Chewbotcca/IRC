b8bf25c34be5353b663462dacb4d0d8bc78aaf62
Chewsterchew <chew@chew.pw>
2017-11-20 22:08:31 -0600

    Add !topspeakers command :O

--- plugins/channel.rb
+++ plugins/channel.rb
@@ -5,6 +5,45 @@ class Channel
   match /resethistory/, method: :clearlog
   match /rquote/, method: :quote
   match /quote (.+)/, method: :userquote
+  match /topspeaker/, method: :top
+
+  def top(m)
+    m.reply 'Gathering top speakers in the channel....'
+    channel = m.channel.to_s[1..m.channel.to_s.length]
+    filename = "data/logs/#{channel}.yaml"
+    unless File.exist?(filename)
+      File.new(filename, 'w+')
+      exconfig = YAML.load_file('data/logs/logs.example.yaml')
+      File.open(filename, 'w') { |f| f.write exconfig.to_yaml }
+      m.reply 'There is no log! No log = No top! Try again, bucko'
+      return
+    end
+    data = false
+    data = YAML.load_file(filename) while data == false
+    count = data['count']
+    users = {}.to_hash
+
+    # Yay let's search every message
+    current = count
+    while current > 1
+      colon = data[current].index(':')
+      user = data[current][0..colon - 1]
+      if users[user].nil?
+        users[user] = 1
+      else
+        users[user] += 1
+      end
+      current -= 1
+    end
+    users = users.sort.sort_by { |_x, y| y }.reverse
+    m.reply "1st: #{Format(:bold, users[0][0].to_s)} with #{Format(:bold, users[0][1].to_s)} messages!"
+    if users.length > 1
+      m.reply "2nd: #{Format(:bold, users[1][0].to_s)} with #{Format(:bold, users[1][1].to_s)} messages!"
+    end
+    if users.length > 2
+      m.reply "3rd: #{Format(:bold, users[2][0].to_s)} with #{Format(:bold, users[2][1].to_s)} messages!"
+    end
+  end
 
   def userquote(m, user)
     user.delete!(' ')
