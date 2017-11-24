8327132549aba54d4dfc1dffd67415cba32b3849
Chewsterchew <chew@chew.pw>
2017-11-23 22:58:57 -0600

    Threw a bunch of garbage commands no one uses into a misc plugin

--- plugins/misc.rb
+++ plugins/misc.rb
@@ -1,6 +1,11 @@
-class EBall
+class Misc
   include Cinch::Plugin
 
+  match /choose (.+)/, method: :choose
+  match /flip/, method: :flip
+  match /coin/, method: :flip
+  match /qr (.+)/, method: :execute
+  match /qrcode (.+)/, method: :execute
   match /8ball/, method: :eball
 
   def eball(m)
@@ -8,5 +13,20 @@ class EBall
     neutraulresponse = Format(:italic, Format(:bold, ['Better not tell you now!', 'Ask again later.', 'Cannot predict now', 'Cooldown enabled! Please try again.', 'Concentrate and ask again.', 'Rhetorical questions can be answered in solo', 'Maybe...'].sample.to_s))
     badresponse = Format(:italic, Format(:bold, Format(:red, ['You\'re kidding, right?', 'Don\'t count on it.', 'In your dreams', 'My reply is no', 'Outlook not so good', 'My disclosed sources say NO', 'One would be wise to think not', 'Very doubtful'].sample.to_s)))
     m.reply "The 8ball says: #{[goodresponse, neutraulresponse, badresponse].sample}"
+  end
+
+  def execute(m, string)
+    m.reply URI.escape("https://api.qrserver.com/v1/create-qr-code/?data=#{string}&size=150x150&.png")
+  end
+
+  def flip(m)
+    first = ['I flipped a coin, and it landed on', 'I threw the coin into the air and it finally landed on', 'I dropped the coin, it landed on'].sample
+    headtails = Format(:bold, Format(:italic, %w[heads tails].sample))
+    m.reply "#{first} #{headtails}"
+  end
+
+  def choose(m, args)
+    args = args.split(',')
+    m.reply args.sample
   end
 end
