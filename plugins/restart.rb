4495435bf21f4428606f6a6b0256ffc40dd83f19
Chewsterchew <chew@chew.pw>
2017-12-04 15:30:21 -0600

    [Fixes #192] Wait 10 seconds before listening to !restart

--- plugins/restart.rb
+++ plugins/restart.rb
@@ -5,31 +5,33 @@ class Restart
   match /update/, method: :update
 
   def restart(m)
-    if m.user.host == CONFIG['ownerhost']
-      m.reply 'Restarting the bot without updating...'
-      sleep 1
-      if CONFIG['bundler'] == true
-        exec('bundle exec ruby bot.rb')
-      else
-        exec('ruby bot.rb')
-      end
+    return if Time.now - STARTTIME < 10
+    unless m.user.host == CONFIG['ownerhost']
+      m.reply "You can't update! (If you are the owner of the bot, you did not configure properly! Otherwise, stop trying to update the bot!)"
+      return
+    end
+    m.reply 'Restarting the bot without updating...'
+    sleep 1
+    if CONFIG['bundler'] == 'true' || CONFIG['bundler'] == true
+      exec('bundle exec ruby bot.rb')
     else
-      m.reply "You can't restart! (If you are the owner of the bot, you did not configure properly! Otherwise, stop trying to restart the bot!)"
+      exec('ruby bot.rb')
     end
   end
 
   def update(m)
-    if m.user.host == CONFIG['ownerhost']
-      m.reply 'Restarting and Updating!'
-      sleep 1
-      `git pull`
-      if CONFIG['bundler'] == true
-        exec('bundle exec ruby bot.rb')
-      else
-        exec('ruby bot.rb')
-      end
-    else
+    return if Time.now - STARTTIME < 10
+    unless m.user.host == CONFIG['ownerhost']
       m.reply "You can't update! (If you are the owner of the bot, you did not configure properly! Otherwise, stop trying to update the bot!)"
+      return
+    end
+    m.reply 'Restarting and Updating!'
+    sleep 1
+    `git pull`
+    if CONFIG['bundler'] == 'true' || CONFIG['bundler'] == true
+      exec('bundle exec ruby bot.rb')
+    else
+      exec('ruby bot.rb')
     end
   end
 end
