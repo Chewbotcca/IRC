969c6e33fead7309a1181af83306bb28bcab26b6
Chewsterchew <chew@chew.pw>
2017-12-04 15:26:02 -0600

    *snaps finger* rubocop *throws on shades*

--- plugins/channel.rb
+++ plugins/channel.rb
@@ -37,14 +37,14 @@ class Channel
     end
     users = users.sort.sort_by { |_x, y| y }.reverse
     sleep 1
-    m.reply "1st: #{Format(:bold, users[0][0].to_s)} with #{Format(:bold, users[0][1].to_s)} messages! #{Format(:bold, "(#{(users[0][1].to_f/count*100).round(2).to_s}%)")}"
+    m.reply "1st: #{Format(:bold, users[0][0].to_s)} with #{Format(:bold, users[0][1].to_s)} messages! #{Format(:bold, "(#{(users[0][1].to_f / count * 100).round(2)}%)")}"
     sleep 1
     if users.length > 1
-      m.reply "2nd: #{Format(:bold, users[1][0].to_s)} with #{Format(:bold, users[1][1].to_s)} messages! #{Format(:bold, "(#{(users[1][1].to_f/count*100).round(2).to_s}%)")}"
+      m.reply "2nd: #{Format(:bold, users[1][0].to_s)} with #{Format(:bold, users[1][1].to_s)} messages! #{Format(:bold, "(#{(users[1][1].to_f / count * 100).round(2)}%)")}"
     end
     sleep 1
     if users.length > 2
-      m.reply "3rd: #{Format(:bold, users[2][0].to_s)} with #{Format(:bold, users[2][1].to_s)} messages! #{Format(:bold, "(#{(users[2][1].to_f/count*100).round(2).to_s}%)")}"
+      m.reply "3rd: #{Format(:bold, users[2][0].to_s)} with #{Format(:bold, users[2][1].to_s)} messages! #{Format(:bold, "(#{(users[2][1].to_f / count * 100).round(2)}%)")}"
     end
     sleep 1
     m.reply "Altogether, everyone has sent #{Format(:bold, count.to_s)} messages! Woah!"
