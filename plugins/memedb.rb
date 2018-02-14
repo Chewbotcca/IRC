963bacfff3c6911d87fbf1ce3cf48d9bede4fd29
Chewsterchew <chew@chew.pw>
2018-02-14 16:26:47 -0600

    organize the memedb code a bit (it's much better I promise)

--- plugins/memedb.rb
+++ plugins/memedb.rb
@@ -5,37 +5,17 @@ class MemeDB
 
   def findthatmeme(m, meme)
     meme.downcase!
-    case meme
-    when 'rickroll'
-      m.reply 'http://chewbotcca.co/memedb/rickroll.gif'
-    when 'vegans'
-      m.reply 'http://chewbotcca.co/memedb/vegans.png'
-    when 'spotad'
-      m.reply 'http://chewbotcca.co/memedb/spotad.jpg'
-    when 'petpet'
-      m.reply 'http://chewbotcca.co/memedb/petpet.jpg'
-    when 'nicememe'
-      m.reply 'http://chewbotcca.co/memedb/nicememe.gif'
-    when 'paycheck'
-      m.reply 'http://chewbotcca.co/memedb/paycheck.JPG'
-    when 'pokesteak'
-      m.reply 'http://chewbotcca.co/memedb/pokesteak.JPG'
-    when 'losthope'
-      m.reply 'http://chewbotcca.co/memedb/losthope.png'
-    when 'timetostop'
-      m.reply 'http://chewbotcca.co/memedb/timetostop.gif'
-    when 'trap'
-      m.reply 'http://chewbotcca.co/memedb/trap.jpeg'
-    when 'triggered'
-      m.reply 'https://chewbotcca.co/memedb/triggered.gif'
-    when 'noot'
-      m.reply 'https://chewbotcca.co/memedb/noot.gif'
-    when 'iplayedmyself'
-      m.reply 'https://chewbotcca.co/memedb/iplayedmyself.png'
-    when 'submit'
-      m.reply 'You can submit a meme here: http://goo.gl/forms/BRMomYVizsY7SqOg2'
-    else
+    prefix = ''
+    prefix = '.gif' if %w[rickroll nicememe timetostop triggered noot].include? meme
+    prefix = '.png' if %w[vegans losthope iplayedmyself].include? meme
+    prefix = '.jpg' if %w[spotad petpet].include? meme
+    prefix = '.JPG' if %w[paycheck pokesteak].include? meme
+    prefix = '.jpeg' if %w[trap].include? meme
+    m.reply 'You can submit a meme here: http://goo.gl/forms/BRMomYVizsY7SqOg2' if meme == 'submit'
+    if prefix == ''
       m.reply "This meme doesn't exist! Make sure you spell the meme name right. Here is a list of the current memes: `rickroll, vegans, spotad, petpet, nicememe, paycheck, pokesteak, losthope, timetostop, trap, triggered, noot, iplayedmyself`"
+    else
+      m.reply "http://chewbotcca.co/memedb/#{meme}#{prefix}"
     end
   end
 end
