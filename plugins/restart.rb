61cc66100d4c0b5dc80c564858a39c8f380fbe47
Chewsterchew <chew@chew.pw>
2018-02-15 18:20:52 -0600

    Add updates command to check for new Chewbotcca updates

--- plugins/restart.rb
+++ plugins/restart.rb
@@ -3,6 +3,7 @@ class Restart
 
   match /restart/, method: :restart
   match /update/, method: :update
+  match /updates/, method: :updates
 
   def restart(m)
     return if Time.now - STARTTIME < 10
@@ -20,6 +21,7 @@ class Restart
   end
 
   def update(m)
+    return if m.params[1][CONFIG['prefix'].length..CONFIG['prefix'].length + 7] == 'updates'
     return if Time.now - STARTTIME < 10
     unless authenticate(m) && checkperm(m, m.user.name, 'restart')
       m.reply "You can't update! (If you are the owner of the bot, you did not configure properly! Otherwise, stop trying to update the bot!)"
@@ -32,6 +34,20 @@ class Restart
       exec('bundle exec ruby bot.rb')
     else
       exec('ruby bot.rb')
+    end
+  end
+
+  def updates(m)
+    unless authenticate(m) && checkperm(m, m.user.name, 'restart')
+      m.reply 'You are not allowed to check for updates!'
+      return
+    end
+    m.reply 'Checking for updates...'
+    response = `git fetch`
+    if response == ''
+      m.reply 'No new updates available.'
+    else
+      m.reply "New updates found! Run `#{CONFIG['prefix']}update` to update"
     end
   end
 end
