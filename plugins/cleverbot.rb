3277ef84a69cea9c12ccfda6a7cb56ba4ac6fb22
Chewsterchew <chew@chew.pw>
2018-02-19 09:40:17 -0600

    Cleverbot now uses previous responses!

--- plugins/cleverbot.rb
+++ plugins/cleverbot.rb
@@ -7,16 +7,14 @@ class Cleverbot
   def cleverbot(m, input)
     File.open("data/logs/#{m.channel.to_s[1..m.channel.to_s.length]}.txt", 'a+') { |f| f.puts("[#{Time.now.to_i}] #{m.user.name}: #{input}") }
     begin
-      response = JSON.parse(RestClient.get(URI.escape("https://www.cleverbot.com/getreply?key=#{CONFIG['cleverbot']}&input=#{input}&cs=&callback=ProcessReply")))
-    rescue
+      response = JSON.parse(RestClient.get(URI.escape("https://www.cleverbot.com/getreply?key=#{CONFIG['cleverbot']}&input=#{input}&cs=#{CONFIG['cs']}")))
+    rescue StandardError
       m.reply 'An error occured! Make sure you have a Cleverbot API key!'
       return
     end
-    # CS will be used eventually!!
     CONFIG['cs'] = response['cs']
     File.open('config.yaml', 'w') { |f| f.write CONFIG.to_yaml }
-    on = response['interaction_count']
-    interaction = "interaction_#{on}_other"
+    interaction = 'interaction_1_other'
     m.reply "Cleverbot says: #{response[interaction]}"
     File.open("data/logs/#{m.channel.to_s[1..m.channel.to_s.length]}.txt", 'a+') { |f| f.puts("[#{Time.now.to_i}] [BOT]Cleverbot: #{response[interaction]}") }
   end
