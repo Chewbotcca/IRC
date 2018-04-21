28ac232d75ef81a286f63c51bba5d250acc1e316
Chewsterchew <chew@chew.pw>
2018-04-21 16:11:06 -0500

    Migrate fancy memedb over here

--- plugins/memedb.rb
+++ plugins/memedb.rb
@@ -5,17 +5,26 @@ class MemeDB
 
   def findthatmeme(m, meme)
     meme.downcase!
-    prefix = ''
-    prefix = '.gif' if %w[rickroll nicememe timetostop triggered noot].include? meme
-    prefix = '.png' if %w[vegans losthope iplayedmyself nottheadmin pineapplepringle].include? meme
-    prefix = '.jpg' if %w[spotad petpet youarerobot].include? meme
-    prefix = '.JPG' if %w[paycheck pokesteak].include? meme
-    prefix = '.jpeg' if %w[trap].include? meme
-    m.reply 'You can submit a meme here: http://goo.gl/forms/BRMomYVizsY7SqOg2' if meme == 'submit'
-    if prefix == ''
-      m.reply "This meme doesn't exist! Make sure you spell the meme name right. Here is a list of the current memes: `rickroll, vegans, spotad, petpet, nicememe, paycheck, pokesteak, losthope, timetostop, trap, triggered, noot, iplayedmyself, nottheadmin, pineapplepringle, youarerobot`"
-    else
-      m.reply "http://chewbotcca.co/memedb/#{meme}#{prefix}"
+    if meme == 'submit'
+      m.reply 'You can submit a meme here: http://goo.gl/forms/BRMomYVizsY7SqOg2'
+      return
+    end
+    memelist = JSON.parse(RestClient.get('http://chewbotcca.co/memedb/memes.json'))
+    memers = []
+    (0..memelist.length - 1).each do |i|
+      memers[memers.length] = memelist[i]['Meme']
+    end
+    memer = JSON.parse(RestClient.get("http://api.chew.pro/chewbotcca/memedb/#{meme}"))
+    if memer['meme'] == 'Invalid Meme'
+      m.reply "This meme doesn't exist! Make sure you spell the meme name right."
+      sleep 1
+      m.reply "Here is a list of the current memes: `#{memers.join(', ')}`"
+      return
+    end
+    begin
+      m.reply memer['url']
+    rescue RestClient::BadRequest
+      m.reply 'There was an error retrieving the meme!'
     end
   end
 end
